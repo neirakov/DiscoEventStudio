840a2ff94107c7ced121c3a6f08a2a3d07afc331
Alexander Neira <neira@Alex_PC>
2024-06-07 16:03:38 +0200

    Añadida funcionalidad "agregar empleado" a la pestaña de personal.

--- src/model/database/DAO/PersonalDAOMySQL.java
+++ src/model/database/DAO/PersonalDAOMySQL.java
@@ -4,10 +4,62 @@
  */
 package model.database.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import model.database.ConexionBD;
+import model.database.Personal;
+
 /**
  *
  * @author neira
  */
-public class PersonalDAOMySQL {
+public class PersonalDAOMySQL implements PersonalDAO {
+
+    @Override
+    public ArrayList<Personal> mostrarPersonal() {
+        ArrayList<Personal> personalLista = new ArrayList<>();
+        String query = "SELECT * FROM Empleados;";
+        try (Connection connection = ConexionBD.getConnection(); Statement stm = connection.createStatement();) {
+            ResultSet resultSet = stm.executeQuery(query);
+            while (resultSet.next()) {
+                int idEmpleado = resultSet.getInt("id_empleado");
+                String nombreEmpleado = resultSet.getString("nombre");
+                String apellidosEmpleado = resultSet.getString("apellidos");
+                String direccionEmpleado = resultSet.getString("direccion");
+                String telefonoEmpleado = resultSet.getString("telefono");
+                Personal personal = new Personal(idEmpleado, nombreEmpleado, apellidosEmpleado,direccionEmpleado,telefonoEmpleado);
+                personalLista.add(personal);
+                System.out.println(personal);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+        }
+        return personalLista;
+    }
+
+    @Override
+    public void eliminarEmpleado(int idEmpleado) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void agregarEmpleado(Personal personal) {
+        String sql = "INSERT INTO Empleados (nombre, apellidos, direccion, telefono) VALUES (?, ?, ?, ?)";
+        try (Connection connection = ConexionBD.getConnection();
+             PreparedStatement pst = connection.prepareStatement(sql)) {
+            
+            pst.setString(1, personal.getNombre());
+            pst.setString(2, personal.getApellidos());
+            pst.setString(3, personal.getDireccion());
+            pst.setString(4, personal.getTelefono());
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
     
 }
